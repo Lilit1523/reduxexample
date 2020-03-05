010af5760b55164895ecab7db15d787eababd8db
Lilit.Arakelyan <Lilit.Arakelyan@forextime.com>
2020-03-05 12:00:57 +0400

    updated posts with material ui

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../action/postAction'
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import { fetchPosts } from '../action/postAction';
+
 
 class Posts extends Component {
 	componentWillMount() {
@@ -9,17 +13,23 @@ class Posts extends Component {
 
 	render() {
 		const postItems = this.props.posts.map(post => (
-			<div key={post.id}>
-				<h3>{post.title}</h3>
-				<p>{post.body}</p>
-			</div>
+			<Grid key={post.id}>
+				<Typography component="h1" variant="h5">
+					{post.title}
+				</Typography>
+				<Typography component="p">
+					{post.body}
+				</Typography>
+			</Grid>
 		));
 
 		return (
-			<div>
-				<h1>Posts</h1>
+			<Container component="main" maxWidth="sm">
+				<Typography component="h1" variant="h1">
+					Posts
+				</Typography>
 				{ postItems }
-			</div>
+			</Container>
 		)
 	}
 }
