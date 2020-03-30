499646dd9f2a9f0ebb877b8f67c7d0d5dfa6b9e9
Lilit.Arakelyan <Lilit.Arakelyan@forextime.com>
2020-03-30 10:25:15 +0400

    add storybook and created stories to react project

---
+++ src/components/layout/header.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'; 
+import Header from './Header';
+
+export default {
+    title: 'Header',
+    component: Header,
+};
+
+export const SiteHeader = () => (
+    <Router>
+        <Header />
+    </Router>
+);
